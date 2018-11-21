529bf6fc0dfe7452624ec6a6de38536502fc78b0
Ma Siyuan <geoms@nus.edu.sg>
2018-11-21 18:02:03 +0800

    first commit

---
+++ resources/tpl/viewer/nls/pl/template.js
@@ -0,0 +1,68 @@
+define({
+  "viewer": {
+    "common": {
+      "close": "Zamknij"
+    },
+    "loading": {
+      "long": "Inicjalizacja aplikacji",
+      "long2": "Dziękujemy za cierpliwość",
+      "failButton": "Ponownie wczytaj aplikację"
+    },
+    "signin": {
+      "title": "Wymagane jest uwierzytelnienie",
+      "explainViewer": "Aby uzyskać dostęp do aplikacji, zaloguj się na konto w portalu %PORTAL_LINK%.",
+      "explainBuilder": "Aby skonfigurować aplikację, zaloguj się na konto w portalu %PORTAL_LINK%."
+    },
+    "errors": {
+      "boxTitle": "Wystąpił błąd",
+      "invalidConfig": "Nieprawidłowa konfiguracja",
+      "invalidConfigNoApp": "Identyfikator aplikacji tworzenia map internetowych nie został określony w pliku index.html.",
+      "invalidConfigNoAppDev": "W parametrach adresu URL nie określono identyfikatora aplikacji map internetowych (?appid=). W trybie deweloperskim konfiguracja parametru appid w pliku index.html jest ignorowana.",
+      "unspecifiedConfigOwner": "Nie skonfigurowano autoryzowanego właściciela.",
+      "invalidConfigOwner": "Właściciel narracji nie jest autoryzowany.",
+      "createMap": "Nie można utworzyć mapy",
+      "invalidApp": "%TPL_NAME% nie istnieje lub jest niedostępna.",
+      "appLoadingFail": "Wystąpił pewien problem, nie wczytano prawidłowo %TPL_NAME%.",
+      "notConfiguredDesktop": "Narracja nie została jeszcze skonfigurowana.",
+      "notConfiguredMobile": "Kreator %TPL_NAME% nie jest obsługiwany dla wyświetlacza w tym rozmiarze. Jeśli to możliwe, zmień rozmiar okna przeglądarki, aby uzyskać dostęp do kreatora, lub buduj narrację na urządzeniu z większym ekranem.",
+      "notConfiguredMobile2": "Aby korzystać z kreatora %TPL_NAME%, obróć urządzenie do orientacji poziomej.",
+      "notAuthorized": "Nie masz uprawnień dostępu do tej narracji",
+      "notAuthorizedBuilder": "Nie masz uprawnień do korzystania z kreatora %TPL_NAME%.",
+      "noBuilderIE": "Kreator nie jest obsługiwany przez przeglądarkę Internet Explorer w wersji starszej niż %VERSION%. %UPGRADE%",
+      "noViewerIE": "Ta narracja nie jest obsługiwana przez przeglądarkę Internet Explorer w wersji starszej niż %VERSION%. %UPGRADE%",
+      "noViewerIE2": "Próbujesz wyświetlić tę narrację za pomocą starszej, nieobsługiwanej przeglądarki. Niektóre funkcje mogą nie działać lub mogą wystąpić inne nieoczekiwane problemy. Zalecamy zaktualizowanie przeglądarki do programu Internet Explorer 11 lub użycie innej przeglądarki, na przykład Chrome.",
+      "noViewerIE3": "Pod koniec 2017 roku ta narracja nie będzie się już wczytywała w tej przeglądarce. Do jej wyświetlenia konieczne będzie użycie obsługiwanej przeglądarki.",
+      "upgradeBrowser": "<a href='http://browsehappy.com/' target='_blank'>Prosimy o zaktualizowanie przeglądarki</a>.",
+      "mapLoadingFail": "Wystąpił pewien problem, nie wczytano mapy prawidłowo.",
+      "signOut": "Wyloguj się",
+      "attention": "Uwaga!",
+      "noBuilderEdge": "Krótkie listy aplikacji Story Map — Krótka lista można wyświetlać w każdej przeglądarce internetowej. Należy jednak pamiętać, że kreator tej aplikacji nie jest obsługiwany w przeglądarce Windows Edge. Do tworzenia i edycji krótkich list należy używać innych przeglądarek.",
+      "noBuilderAllIE": "Story Map — Krótka lista to szablon, który można wyświetlać w każdej przeglądarce internetowej. Należy jednak pamiętać, że kreator tej aplikacji nie jest obsługiwany w przeglądarkach Microsoft Edge i Internet Explorer. Do tworzenia i edycji krótkich list należy używać innych przeglądarek."
+    },
+    "headerFromCommon": {
+      "storymapsText": "Mapa narracyjna",
+      "builderButton": "Edytuj",
+      "facebookTooltip": "Udostępnij na Facebooku",
+      "twitterTooltip": "Udostępnij na Twitterze",
+      "bitlyTooltip": "Utwórz skrócony link",
+      "templateTitle": "Skonfiguruj tytuł szablonu",
+      "templateSubtitle": "Skonfiguruj podtytuł szablonu",
+      "share": "Udostępnianie"
+    },
+    "shareFromCommon": {
+      "copy": "Kopiuj",
+      "copied": "Skopiowane",
+      "open": "Otwórz",
+      "embed": "Osadzono na stronie internetowej",
+      "embedExplain": "Aby umieścić Dziennik na stronie internetowej, skorzystaj z poniższego kodu HTML.",
+      "size": "Rozmiar (szerokość/wysokość):",
+      "autoplayLabel": "Tryb automatycznego odtwarzania",
+      "autoplayExplain1": "W trybie automatycznego odtwarzania narracja jest prezentowana w stałym tempie. Ten tryb doskonale nadaje się do monitora kiosku internetowego lub wyświetlacza publicznego, ale należy pamiętać, że w innych sytuacjach może pogorszyć czytelność narracji. Ta funkcja nie jest obsługiwana na małych wyświetlaczach.",
+      "autoplayExplain2": "Po włączeniu tego trybu udostępniane są elementy sterujące do włączania odtwarzania/wstrzymywania narracji oraz do regulacji szybkości nawigacji."
+    },
+    "general": {
+      "noPlaces": "W tym obszarze nie ma żadnych miejsc. Pomniejsz lub kliknij przycisk Start, aby wyświetlić miejsca.",
+      "start": "Uruchom"
+    }
+  }
+});
